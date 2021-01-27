c8b90ccef00f6ab4d9c6519b5187613428eeaf5f
onkar-code <cf19onkar.singh@iesjoandaustria.org>
2021-01-27 20:16:11 +0100

    añadido drops, auto_increment y formato

--- Script_DB.sql
+++ Script_DB.sql
@@ -1,45 +1,52 @@
-create table Producte (
-  id serial primary key,
+Drop table Client;
+Drop table Producte;
+Drop table Fotografies;
+
+CREATE TABLE Client ( 
+  id smallint primary key auto_increment,
+  nom varchar(30) not null, 
+  cognom1 varchar(30) not null, 
+  cognom2 varchar(30), 
+  usuari varchar(30) not null, 
+  password varchar(50) not null, 
+  email varchar(30) not null 
+); 
+
+CREATE TABLE Producte (
+  id smallint primary key auto_increment,
   nom varchar(30) not null,
   descripcio varchar(100) not null,
-  preu numeric(5,2) not null check > 0,
-  altura int check > 0,
-  ample int check > 0,
-  fons int check > 0,
-  pes numeric(5,3) not null check > 0,
+  preu numeric(5,2) not null check (preu > 0),
+  altura int check  (altura > 0),
+  ample int check (ample > 0),
+  fons int check (fons > 0),
+  pes numeric(5,3) not null check (pes > 0),
   categoria varchar(30) not null,
   data_publicacio Date not null,
   visites int default 0,
-  idClient int not null,
+  idClient smallint not null,
   numFotos int default 0
 );
 
 ALTER TABLE Producte
-ADD CONSTRAINT CHK_Fotos CHECK (numFotos < 4);
+  ADD CONSTRAINT CHK_Fotos 
+  CHECK (numFotos < 4);
 
 ALTER TABLE Producte
-ADD CONSTRAINT CHK_Categoria CHECK (categoria = 'Libros' or categoria = 'Moviles' or categoria = 'Videojuegos');
+  ADD CONSTRAINT CHK_Categoria 
+  CHECK (categoria = 'Libros' or categoria = 'Moviles' or categoria = 'Videojuegos');
 
 ALTER TABLE Producte
-ADD CONSTRAINT FK_Client
-FOREIGN KEY (idClient) REFERENCES Client(id);
+  ADD CONSTRAINT FK_Client
+  FOREIGN KEY (idClient) REFERENCES Client(id)
+  ON DELETE CASCADE;
 
-create table Client (
-  id serial primary key,
-  nom varchar(30) not null,
-  cognom1 varchar(30) not null,
-  cognom2 varchar(30),
-  usuari varchar(30) not null,
-  password varchar(50) not null,
-  email varchar(30) not null
-);
-
-create table Fotografies (
-  idFoto serial primary key,
-  idProducte int not null,
-  url varchar(100)
-);
+CREATE TABLE Fotografies ( 
+  idFoto smallint primary key auto_increment,
+  idProducte smallint not null,
+  url varchar(100) 
+); 
 
 ALTER TABLE Fotografies
-ADD CONSTRAINT FK_Producte
-FOREIGN KEY (idProducte) REFERENCES Producte(id);
+  ADD CONSTRAINT FK_Producte
+  FOREIGN KEY (idProducte) REFERENCES Producte(id);
